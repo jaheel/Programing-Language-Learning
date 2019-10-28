c9772d0dc14d02b0ff1b0e9df34286de05efb839
Alex <xufanxin@sina.cn>
2019-10-28 20:27:28 +0800

    Create Adapter pattern.cs

---
+++ PatternNote/Adapter-and-Facade-pattern/Adapter pattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PatternTest
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            IThreeHole threehole = new PowerAdapter();
+            threehole.Request();
+            Console.Read();
+        }
+        
+        public interface IThreeHole
+        {
+            void Request();
+        }
+
+        public abstract class TwoHole
+        {
+            public void SpecificRequest()
+            {
+                Console.WriteLine("Tow Hole");
+            }
+        }
+
+        public class PowerAdapter : TwoHole, IThreeHole
+        {
+            public void Request()
+            {
+                this.SpecificRequest();
+            }
+        }
+    }
+
+   
+    
+
+}
